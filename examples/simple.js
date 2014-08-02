33300cf9d5cb2ad549e8f312d46f1f55e1d8f6ca
Partha R <parthasarathi.ramanujam@gmail.com>
2014-08-02 15:38:06 +0530

    added simple webserver example

---
+++ examples/simple.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var webexapp = require('../lib'),
+    WebServer = webexapp.WebServer;
+
+var server;
+
+webexapp.setWebserverConfig({workers: 1}); // set number of workers to 1
+server = new WebServer('simple-server');
+server.use('/hello', function (req, res) {
+    res.send('hello world');
+});
+server.run();
