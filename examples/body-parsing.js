aead39707e74f08a06e8190bf83e887bb1e2c2db
Partha R <parthasarathi.ramanujam@gmail.com>
2014-08-02 18:46:13 +0530

    body-parsing fix and example

---
+++ examples/body-parsing.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var webexapp = require('../lib'),
+    express = require('express'),
+    WebServer = webexapp.WebServer;
+
+var server, app;
+
+webexapp.setWebserverConfig({workers: 1}); // set number of workers to 1
+server = new WebServer('simple-server');
+
+app = express();
+app.post('/json', function (req, res) {
+    res.send(JSON.stringify(req.body));
+});
+app.post('/urlencoded', function (req, res) {
+    res.send(JSON.stringify(req.body));
+});
+app.post('/multipart', function (req, res) {
+    // raw-stream available as req.rawBodyStream - pipe it something like busboy
+    res.send('OK');
+});
+
+server.use('/', app);
+server.run();
+
+// POST to /json & /urlencoded
